dc25e7f336976ca9713c87065bea686784cf2c2d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-03 13:49:35 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col mt-20 justify-between ">
+      <main className="flex min-h-screen flex-col mt-20 items-center ">
+        <h1 className="mb-10 text-gray-900 font-bold text-2xl">
+          Espace administrateur
+        </h1>
         <ConnexionForm title="Connectez-vous à votre espace administrateur" />
       </main>
       <Footer />
