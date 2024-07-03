e5ab0c9c73c46087de36d03c49b1e6beafd6c855
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-03 20:02:05 +0200

    Update page.tsx

--- src/app/espace-administrateur/partenaires/page.tsx
+++ src/app/espace-administrateur/partenaires/page.tsx
@@ -3,7 +3,7 @@ import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
 const Page = () => {
   return (
     <SideBarLayout>
-      <h1>Patenaires</h1>
+      <h1 className="text-2xl font-bold text-gray-900">Patenaires</h1>
     </SideBarLayout>
   );
 };
