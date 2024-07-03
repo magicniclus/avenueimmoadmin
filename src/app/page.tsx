0d4b24032e40d62a720d4a089266f251c247a281
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-03 11:23:22 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Footer from "@/components/tailwindUi/footer/Footer";
+import ConnexionForm from "@/components/tailwindUi/form/Connexion";
 import Header from "@/components/tailwindUi/header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="flex min-h-screen flex-col mt-20 justify-between ">
+        <ConnexionForm title="Connectez-vous à votre espace administrateur" />
+      </main>
       <Footer />
     </>
   );
