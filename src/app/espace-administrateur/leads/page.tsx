b7db42b16f3cdc2923416e8969d54a1150af5ef1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-03 13:45:45 +0200

    Create page.tsx

---
+++ src/app/espace-administrateur/leads/page.tsx
@@ -0,0 +1,11 @@
+import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
+
+const Page = () => {
+  return (
+    <SideBarLayout>
+      <h1>Leads</h1>
+    </SideBarLayout>
+  );
+};
+
+export default Page;
