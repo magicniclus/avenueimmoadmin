b5021993fa3c631ff24c15b892595c34d74c3c20
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-03 18:02:17 +0200

    Update page.tsx

--- src/app/espace-administrateur/leads/page.tsx
+++ src/app/espace-administrateur/leads/page.tsx
@@ -1,9 +1,46 @@
+"use client";
+import { AllDataEstimation } from "@/components/dataTable.tsx/AllDataEstimation";
 import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
+import { getAllData } from "@/firebase/database";
+import { get, getDatabase, ref } from "firebase/database";
+import { useEffect, useState } from "react";
 
-const Page = () => {
+export const Page = () => {
+  const testFirebase = async () => {
+    try {
+      const dbRef = ref(getDatabase(), "/estimations");
+      const snapshot = await get(dbRef);
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    } catch (error) {
+      console.error("Error testing Firebase configuration:", error);
+    }
+  };
+
+  testFirebase();
+  const [leads, setLeads] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchLeads = async () => {
+      try {
+        const data = await getAllData("/estimations");
+        console.log("Fetched leads:", data);
+        setLeads(data);
+      } catch (error) {
+        console.error("Error getting leads:", error);
+      }
+    };
+
+    fetchLeads();
+  }, []);
+
   return (
     <SideBarLayout>
       <h1>Leads</h1>
+      <AllDataEstimation />
     </SideBarLayout>
   );
 };
