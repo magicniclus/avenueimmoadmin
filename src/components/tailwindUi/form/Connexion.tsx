b097b1df85fbf17b3094768d5e7cb704493cc9ef
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-03 11:23:30 +0200

    Update Connexion.tsx

--- src/components/tailwindUi/form/Connexion.tsx
+++ src/components/tailwindUi/form/Connexion.tsx
@@ -50,7 +50,7 @@ const ConnexionForm: React.FC<ConnexionFormProps> = ({ title, href }) => {
 
     try {
       await signIn(email, password);
-      router.push(href || "/user/tableau-de-bord");
+      router.push(href || "/espace-administrateur/tableau-de-bord");
       // Redirection ou autres actions après connexion réussie
     } catch (error) {
       setError("Échec de la connexion. Veuillez vérifier vos informations.");
@@ -59,19 +59,9 @@ const ConnexionForm: React.FC<ConnexionFormProps> = ({ title, href }) => {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex flex-col justify-center px-6 lg:px-8">
       <Toaster /> {/* Assurez-vous que le composant Toaster est inclus */}
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          {title ? title : "Connectez-vous à votre espace"}
-        </h2>
-      </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
