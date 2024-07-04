84c8ac5442189bde3ab45448ce40f7921d1c3916
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-04 17:08:20 +0200

    Update DrawerEstimationContent.tsx

--- src/components/tailwindUi/drawer/DrawerEstimationContent.tsx
+++ src/components/tailwindUi/drawer/DrawerEstimationContent.tsx
@@ -1,12 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { getDataById } from "@/firebase/database";
+"use client";
+
+import { getAllData, getDataById } from "@/firebase/database";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -23,8 +18,16 @@ interface Estimation {
   partenaire?: string;
 }
 
+interface Agent {
+  id: string;
+  informations: {
+    prenom: string;
+    nom: string;
+    secteur: { key: string; value: string }[];
+  };
+}
+
 const fetchEstimationById = async (id: string): Promise<Estimation | null> => {
-  console.log("Fetching estimation by ID:", id);
   try {
     const result = await getDataById("/estimations", id);
     if (result) {
@@ -37,6 +40,20 @@ const fetchEstimationById = async (id: string): Promise<Estimation | null> => {
   }
 };
 
+const fetchAllAgents = async (): Promise<Agent[]> => {
+  try {
+    const result = await getAllData("/agents");
+    const agents = Object.keys(result).map((key) => ({
+      id: key,
+      ...result[key],
+    }));
+    return agents;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des agents:", error);
+    return [];
+  }
+};
+
 const DrawerEstimationContent: React.FC = () => {
   const router = useRouter();
   const pathname = usePathname();
@@ -45,14 +62,16 @@ const DrawerEstimationContent: React.FC = () => {
 
   const [estimation, setEstimation] = useState<Estimation | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [partenaire, setPartenaire] = useState<string | undefined>(undefined);
+  const [agents, setAgents] = useState<Agent[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       if (id) {
         const data = await fetchEstimationById(id);
-        console.log("Data:", data);
         if (data) {
           setEstimation(data);
+          setPartenaire(data.partenaire);
         } else {
           setError("Estimation non trouvée");
         }
@@ -62,19 +81,15 @@ const DrawerEstimationContent: React.FC = () => {
     fetchData();
   }, [id]);
 
-  if (error) return <div>Erreur: {error}</div>;
-  if (!estimation)
-    return (
-      <div className="flex justify-center items-center h-screen w-screen bg-white">
-        <div className="text-center">
-          <img
-            src="/favicon.png"
-            alt="logo"
-            className="animate-pulse w-20 h-20"
-          />
-        </div>
-      </div>
-    );
+  useEffect(() => {
+    const fetchAgentsData = async () => {
+      const agentsData = await fetchAllAgents();
+      setAgents(agentsData);
+      console.log("Agents data:", agentsData);
+    };
+
+    fetchAgentsData();
+  }, []);
 
   if (error) return <div>Erreur: {error}</div>;
   if (!estimation)
@@ -89,6 +104,63 @@ const DrawerEstimationContent: React.FC = () => {
         </div>
       </div>
     );
+
+  const renderAgentsOptions = () => {
+    const defaultOption = (
+      <option key="default" value="" disabled className="text-gray-500">
+        Attribuer à un agent
+      </option>
+    );
+
+    if (!partenaire) {
+      return [
+        defaultOption,
+        ...agents.map((agent) => (
+          <option key={agent.id} value={agent.id}>
+            {`${agent.informations.prenom} ${agent.informations.nom} - ${
+              Array.isArray(agent.informations.secteur)
+                ? agent.informations.secteur
+                    .map((secteur) => secteur.value)
+                    .join(", ")
+                : Object.values(agent.informations.secteur).join(", ")
+            }`}
+          </option>
+        )),
+      ];
+    }
+
+    const selectedAgent = agents.find((agent) => agent.id === partenaire);
+
+    return [
+      selectedAgent && (
+        <option key={selectedAgent.id} value={selectedAgent.id}>
+          {`${selectedAgent.informations.prenom} ${
+            selectedAgent.informations.nom
+          } - ${
+            Array.isArray(selectedAgent.informations.secteur)
+              ? selectedAgent.informations.secteur
+                  .map((secteur) => secteur.value)
+                  .join(", ")
+              : Object.values(selectedAgent.informations.secteur).join(", ")
+          }`}
+        </option>
+      ),
+      defaultOption,
+      ...agents
+        .filter((agent) => agent.id !== partenaire)
+        .map((agent) => (
+          <option key={agent.id} value={agent.id}>
+            {`${agent.informations.prenom} ${agent.informations.nom} - ${
+              Array.isArray(agent.informations.secteur)
+                ? agent.informations.secteur
+                    .map((secteur) => secteur.value)
+                    .join(", ")
+                : Object.values(agent.informations.secteur).join(", ")
+            }`}
+          </option>
+        )),
+    ];
+  };
 
   return (
     <div>
@@ -142,7 +214,7 @@ const DrawerEstimationContent: React.FC = () => {
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
-              Prix Prévu
+              Prix estimer
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
               {new Intl.NumberFormat("fr-FR", {
@@ -167,25 +239,24 @@ const DrawerEstimationContent: React.FC = () => {
               {estimation?.assigned ? "Oui" : "Non"}
             </dd>
           </div>
-          {
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm font-medium leading-6 text-gray-900">
-                Partenaire
-              </dt>
-              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                <Select>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Theme" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="light">Light</SelectItem>
-                    <SelectItem value="dark">Dark</SelectItem>
-                    <SelectItem value="system">System</SelectItem>
-                  </SelectContent>
-                </Select>
-              </dd>
-            </div>
-          }
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Partenaire
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <div>
+                <select
+                  id="partenaire"
+                  name="partenaire"
+                  className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={partenaire || ""}
+                  onChange={(e) => setPartenaire(e.target.value)}
+                >
+                  {renderAgentsOptions()}
+                </select>
+              </div>
+            </dd>
+          </div>
         </dl>
       </div>
     </div>
