48d49d1b4e5837306bd74a784c7cff469863f006
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-08 10:15:15 +0200

    Update page.tsx

--- src/app/espace-administrateur/leads/page.tsx
+++ src/app/espace-administrateur/leads/page.tsx
@@ -3,39 +3,36 @@ import { AllDataEstimation } from "@/components/dataTable.tsx/AllDataEstimation"
 import Drawer from "@/components/tailwindUi/drawer/Drawer";
 import DrawerEstimationContent from "@/components/tailwindUi/drawer/DrawerEstimationContent";
 import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
-import { getAllData } from "@/firebase/database";
-import { get, getDatabase, ref } from "firebase/database";
-import { useEffect, useState } from "react";
 
 export const Page = () => {
-  const testFirebase = async () => {
-    try {
-      const dbRef = ref(getDatabase(), "/estimations");
-      const snapshot = await get(dbRef);
-      if (snapshot.exists()) {
-      } else {
-        console.log("No data available");
-      }
-    } catch (error) {
-      console.error("Error testing Firebase configuration:", error);
-    }
-  };
+  // const testFirebase = async () => {
+  //   try {
+  //     const dbRef = ref(getDatabase(), "/estimations");
+  //     const snapshot = await get(dbRef);
+  //     if (snapshot.exists()) {
+  //     } else {
+  //       console.log("No data available");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error testing Firebase configuration:", error);
+  //   }
+  // };
 
-  testFirebase();
-  const [leads, setLeads] = useState<any>(null);
+  // testFirebase();
+  // const [leads, setLeads] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchLeads = async () => {
-      try {
-        const data = await getAllData("/estimations");
-        setLeads(data);
-      } catch (error) {
-        console.error("Error getting leads:", error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchLeads = async () => {
+  //     try {
+  //       const data = await getAllData("/estimations");
+  //       setLeads(data);
+  //     } catch (error) {
+  //       console.error("Error getting leads:", error);
+  //     }
+  //   };
 
-    fetchLeads();
-  }, []);
+  //   fetchLeads();
+  // }, []);
 
   return (
     <div className="relative">
