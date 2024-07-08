7d2f084b012a59bee87f589ac02f0b8b5c52d284
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-08 11:17:54 +0200

    Update page.tsx

--- src/app/espace-administrateur/partenaires/page.tsx
+++ src/app/espace-administrateur/partenaires/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { AllDataPartenaire } from "@/components/dataTable.tsx/AllDataPartenaire";
 import Drawer from "@/components/tailwindUi/drawer/Drawer";
-import DrawerEstimationContent from "@/components/tailwindUi/drawer/DrawerEstimationContent";
+import DrawerPartenaireContent from "@/components/tailwindUi/drawer/DrawerPartenaireContent";
 import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
 
 const Page = () => {
   return (
     <div className="relative">
       <Drawer>
-        <DrawerEstimationContent />
+        <DrawerPartenaireContent />
       </Drawer>
       <SideBarLayout>
         <h1 className="text-2xl font-bold text-gray-900">Partenaires</h1>
