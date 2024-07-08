85ce55b4cf3540b3787fba5317e2dbcf46070e20
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-08 10:15:19 +0200

    Create AllDataPartenaire.tsx

---
+++ src/components/dataTable.tsx/AllDataPartenaire.tsx
@@ -0,0 +1,327 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { database } from "@/firebase/firebase.config";
+import { setDrawerOpen } from "@/redux/drawerSlice";
+import { RootState } from "@/redux/store";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { onValue, ref } from "firebase/database";
+import { ArrowUpDown, ChevronDown } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export type Estimation = {
+  id: string;
+  adresse: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  predictedPrice: number;
+  date: string;
+  assigned?: boolean;
+};
+
+export const columns: ColumnDef<Estimation>[] = [
+  {
+    accessorKey: "adresse",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Adresse
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("adresse")}</div>,
+  },
+  {
+    accessorKey: "firstName",
+    header: "Prénom",
+    cell: ({ row }) => <div>{row.getValue("firstName")}</div>,
+  },
+  {
+    accessorKey: "lastName",
+    header: "Nom",
+    cell: ({ row }) => <div>{row.getValue("lastName")}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    cell: ({ row }) => <div>{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "phone",
+    header: "Téléphone",
+    cell: ({ row }) => <div>{row.getValue("phone")}</div>,
+  },
+  {
+    accessorKey: "predictedPrice",
+    header: () => <div className="text-right">Prix Prévu</div>,
+    cell: ({ row }) => {
+      const predictedPrice = parseFloat(row.getValue("predictedPrice"));
+      const formatted = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+      }).format(predictedPrice);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    cell: ({ row }) => <div>{row.getValue("date")}</div>,
+  },
+  {
+    accessorKey: "assigned",
+    header: "Attribué",
+    cell: ({ row }) => (
+      <div
+        className={`${
+          row.getValue("assigned")
+            ? "p-1 rounded-lg bg-blue-300 border border-blue-500 flex justify-center"
+            : "p-1 rounded-lg bg-red-300 border border-red-500 flex justify-center"
+        }`}
+      >
+        {row.getValue("assigned") ? "Oui" : "Non"}
+      </div>
+    ),
+  },
+];
+
+export function AllDataPartenaire() {
+  const dispatch = useDispatch();
+
+  const drawerOpen = useSelector(
+    (state: RootState) => state?.drawer?.drawerOpen
+  );
+
+  const [data, setData] = useState<Estimation[]>([]);
+  const [allId, setAllId] = useState<{ id: string }[]>([]);
+  const [uniqueId, setUniqueId] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const router = useRouter();
+  const pathName = usePathname();
+
+  useEffect(() => {
+    const dataRef = ref(database, "/estimations");
+    const unsubscribe = onValue(
+      dataRef,
+      (snapshot) => {
+        const result = snapshot.val();
+        if (result) {
+          const parsedData = Object.keys(result).map((key) => ({
+            id: key,
+            assigned: result[key].assigned ?? false,
+            ...result[key],
+          }));
+          const showAllId = Object.keys(result).map((key) => ({
+            id: key,
+          }));
+          setAllId(showAllId);
+          setData(parsedData);
+        } else {
+          setAllId([]);
+          setData([]);
+        }
+      },
+      (error) => {
+        setError(error.message);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [globalFilter, setGlobalFilter] = useState<string>("");
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    globalFilterFn: (row, columnId, filterValue) => {
+      return String(row.getValue(columnId))
+        .toLowerCase()
+        .includes(String(filterValue).toLowerCase());
+    },
+    state: {
+      sorting,
+      globalFilter,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  const setOpen = (value: boolean) => {
+    dispatch(setDrawerOpen(value));
+  };
+
+  const handleRowClick = async (index: number) => {
+    setUniqueId(allId[index].id);
+  };
+
+  useEffect(() => {
+    if (uniqueId) {
+      router.push(`?id=${uniqueId}`);
+      setOpen(true);
+    }
+  }, [uniqueId]);
+
+  useEffect(() => {
+    if (!drawerOpen) {
+      router.push(pathName); // Remove query parameter when drawer is closed
+    }
+  }, [drawerOpen, pathName, router]);
+
+  return (
+    <div className="w-full">
+      {error && <p>Erreur: {error}</p>}
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filtrer les données..."
+          value={globalFilter ?? ""}
+          onChange={(event) => setGlobalFilter(event.target.value)}
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Colonnes <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row, index) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  onClick={() => handleRowClick(index)}
+                  className="cursor-pointer"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Aucun résultat.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Précédent
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Suivant
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
